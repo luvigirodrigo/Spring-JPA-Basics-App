9406ed77b674eb4de62723581d9a26fff2a2eca2
Luvigi Priyanjana Rodrigo <luvigirodrigo6@gmail.com>
2020-01-18 20:07:36 +0530

    initial commit

---
+++ spring-jpa-basics-app/src/main/java/com/myorg/JPAapp/SpringJpaBasicsAppApplication.java
@@ -0,0 +1,13 @@
+package com.myorg.JPAapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaBasicsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaBasicsAppApplication.class, args);
+	}
+
+}
---
+++ spring-jpa-basics-app/src/test/java/com/myorg/JPAapp/SpringJpaBasicsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myorg.JPAapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaBasicsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
